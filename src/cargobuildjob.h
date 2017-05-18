21d163afd36c549fdf26a5b0fc3736fea4391e79
Miha Čančula <miha@noughmad.eu>
2017-05-18 23:13:20 +0200

    Organize licenses and license headers

--- src/cargobuildjob.h
+++ src/cargobuildjob.h
@@ -1,30 +1,33 @@
-/************************************************************************
- * KDevelop4 Cargo Buildsystem Support                                 *
- *                                                                      *
- * Copyright 2010 Andreas Pakulat <apaku@gmx.de>                        *
- *                                                                      *
- * This program is free software; you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2 or version 3 of the License, or    *
- * (at your option) any later version.                                  *
- *                                                                      *
- * This program is distributed in the hope that it will be useful, but  *
- * WITHOUT ANY WARRANTY; without even the implied warranty of           *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     *
- * General Public License for more details.                             *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with this program; if not, see <http://www.gnu.org/licenses/>. *
- ************************************************************************/
+/*
+ * This file is part of the Cargo plugin for KDevelop.
+ *
+ * Copyright 2017 Miha Čančula <miha@noughmad.eu>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
 
-#ifndef CUSTOMBUILDJOB_H
-#define CUSTOMBUILDJOB_H
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CARGOBUILDJOB_H
+#define CARGOBUILDJOB_H
 
 #include <outputview/outputjob.h>
 #include <QProcess>
 #include <QUrl>
 
-class Cargo;
+class CargoPlugin;
 namespace KDevelop
 {
 class ProjectBaseItem;
@@ -47,7 +50,7 @@ public:
         NoCommand
     };
 
-    CargoBuildJob( Cargo*, KDevelop::ProjectBaseItem*, const QString& command );
+    CargoBuildJob( CargoPlugin*, KDevelop::ProjectBaseItem*, const QString& command );
     void start() override;
     bool doKill() override;
 
--- src/cargoplugin.cpp
+++ src/cargoplugin.cpp
@@ -1,21 +1,24 @@
-/************************************************************************
- * KDevelop4 Custom Buildsystem Support                                 *
- *                                                                      *
- * Copyright 2010 Andreas Pakulat <apaku@gmx.de>                        *
- *                                                                      *
- * This program is free software; you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2 or version 3 of the License, or    *
- * (at your option) any later version.                                  *
- *                                                                      *
- * This program is distributed in the hope that it will be useful, but  *
- * WITHOUT ANY WARRANTY; without even the implied warranty of           *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     *
- * General Public License for more details.                             *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with this program; if not, see <http://www.gnu.org/licenses/>. *
- ************************************************************************/
+/*
+ * This file is part of the Cargo plugin for KDevelop.
+ *
+ * Copyright 2017 Miha Čančula <miha@noughmad.eu>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "cargoplugin.h"
 
@@ -47,9 +50,9 @@ using KDevelop::IProjectBuilder;
 using KDevelop::IProject;
 using KDevelop::Path;
 
-K_PLUGIN_FACTORY_WITH_JSON(CargoFactory, "kdevcargo.json", registerPlugin<Cargo>(); )
+K_PLUGIN_FACTORY_WITH_JSON(CargoFactory, "kdevcargo.json", registerPlugin<CargoPlugin>(); )
 
-Cargo::Cargo( QObject *parent, const QVariantList & )
+CargoPlugin::CargoPlugin( QObject *parent, const QVariantList & )
     : AbstractFileManagerPlugin( QStringLiteral("kdevcargo"), parent )
 {
     m_configType = new CargoExecutionConfigType();
@@ -57,122 +60,122 @@ Cargo::Cargo( QObject *parent, const QVariantList & )
     core()->runController()->addConfigurationType( m_configType );
 }
 
-Cargo::~Cargo()
+CargoPlugin::~CargoPlugin()
 {
 }
 
-void Cargo::unload()
+void CargoPlugin::unload()
 {
     core()->runController()->removeConfigurationType( m_configType );
     delete m_configType;
     m_configType = nullptr;
 }
 
-bool Cargo::addFilesToTarget( const QList<ProjectFileItem*>&, ProjectTargetItem* )
+bool CargoPlugin::addFilesToTarget( const QList<ProjectFileItem*>&, ProjectTargetItem* )
 {
     return false;
 }
 
-bool Cargo::hasBuildInfo( ProjectBaseItem* ) const
+bool CargoPlugin::hasBuildInfo( ProjectBaseItem* ) const
 {
     return false;
 }
 
-KJob* Cargo::build( ProjectBaseItem* dom )
+KJob* CargoPlugin::build( ProjectBaseItem* dom )
 {
     return new CargoBuildJob( this, dom, QStringLiteral("build") );
 }
 
-Path Cargo::buildDirectory( ProjectBaseItem*  item ) const
+Path CargoPlugin::buildDirectory( ProjectBaseItem*  item ) const
 {
     return item->project()->path();
 }
 
-IProjectBuilder* Cargo::builder() const
+IProjectBuilder* CargoPlugin::builder() const
 {
     return const_cast<IProjectBuilder*>(dynamic_cast<const IProjectBuilder*>(this));
 }
 
-KJob* Cargo::clean( ProjectBaseItem* dom )
+KJob* CargoPlugin::clean( ProjectBaseItem* dom )
 {
     return new CargoBuildJob( this, dom, QStringLiteral("clean") );
 }
 
-KJob* Cargo::configure( IProject* project )
+KJob* CargoPlugin::configure( IProject* project )
 {
     return new CargoBuildJob( this, project->projectItem(), QString() );
 }
 
-ProjectTargetItem* Cargo::createTarget( const QString&, ProjectFolderItem* )
+ProjectTargetItem* CargoPlugin::createTarget( const QString&, ProjectFolderItem* )
 {
     return nullptr;
 }
 
-QHash<QString, QString> Cargo::defines( ProjectBaseItem* ) const
+QHash<QString, QString> CargoPlugin::defines( ProjectBaseItem* ) const
 {
     return {};
 }
 
-IProjectFileManager::Features Cargo::features() const
+IProjectFileManager::Features CargoPlugin::features() const
 {
     return IProjectFileManager::Files | IProjectFileManager::Folders;
 }
 
-ProjectFolderItem* Cargo::createFolderItem( IProject* project,
+ProjectFolderItem* CargoPlugin::createFolderItem( IProject* project,
                     const Path& path, ProjectBaseItem* parent )
 {
     return new ProjectBuildFolderItem( project, path, parent );
 }
 
-Path::List Cargo::includeDirectories( ProjectBaseItem* ) const
+Path::List CargoPlugin::includeDirectories( ProjectBaseItem* ) const
 {
     return {};
 }
 
-Path::List Cargo::frameworkDirectories( ProjectBaseItem* ) const
+Path::List CargoPlugin::frameworkDirectories( ProjectBaseItem* ) const
 {
     return {};
 }
 
-KJob* Cargo::install( KDevelop::ProjectBaseItem* item, const QUrl &installPrefix )
+KJob* CargoPlugin::install( KDevelop::ProjectBaseItem* item, const QUrl &installPrefix )
 {
     auto job = new CargoBuildJob( this, item, QStringLiteral("install") );
     job->setInstallPrefix(installPrefix);
     return job;
 }
 
-KJob* Cargo::prune( IProject* project )
+KJob* CargoPlugin::prune( IProject* project )
 {
     return clean(project->projectItem());
 }
 
-bool Cargo::removeFilesFromTargets( const QList<ProjectFileItem*>& )
+bool CargoPlugin::removeFilesFromTargets( const QList<ProjectFileItem*>& )
 {
     return false;
 }
 
-bool Cargo::removeTarget( ProjectTargetItem* )
+bool CargoPlugin::removeTarget( ProjectTargetItem* )
 {
     return false;
 }
 
-QList<ProjectTargetItem*> Cargo::targets( ProjectFolderItem* ) const
+QList<ProjectTargetItem*> CargoPlugin::targets( ProjectFolderItem* ) const
 {
     return QList<ProjectTargetItem*>();
 }
 
-KConfigGroup Cargo::configuration( IProject* project ) const
+KConfigGroup CargoPlugin::configuration( IProject* project ) const
 {
     Q_UNUSED(project);
     return KConfigGroup();
 }
 
-int Cargo::perProjectConfigPages() const
+int CargoPlugin::perProjectConfigPages() const
 {
     return 0;
 }
 
-KDevelop::ConfigPage* Cargo::perProjectConfigPage(int number, const KDevelop::ProjectConfigOptions& options, QWidget* parent)
+KDevelop::ConfigPage* CargoPlugin::perProjectConfigPage(int number, const KDevelop::ProjectConfigOptions& options, QWidget* parent)
 {
     Q_UNUSED(number);
     Q_UNUSED(options);
@@ -180,13 +183,13 @@ KDevelop::ConfigPage* Cargo::perProjectConfigPage(int number, const KDevelop::Pr
     return nullptr;
 }
 
-QUrl Cargo::executable(KDevelop::ILaunchConfiguration* config, QString& /*error*/) const
+QUrl CargoPlugin::executable(KDevelop::ILaunchConfiguration* config, QString& /*error*/) const
 {
     Q_UNUSED(config);
     return QUrl::fromLocalFile(QStandardPaths::findExecutable("cargo"));
 }
 
-QStringList Cargo::arguments(KDevelop::ILaunchConfiguration* config, QString& /*error*/) const
+QStringList CargoPlugin::arguments(KDevelop::ILaunchConfiguration* config, QString& /*error*/) const
 {
     QStringList ret = {QStringLiteral("run")};
     qWarning() << "Config:" << config->config().entryMap();
@@ -203,33 +206,33 @@ QStringList Cargo::arguments(KDevelop::ILaunchConfiguration* config, QString& /*
     return ret;
 }
 
-KJob* Cargo::dependencyJob(KDevelop::ILaunchConfiguration* config) const
+KJob* CargoPlugin::dependencyJob(KDevelop::ILaunchConfiguration* config) const
 {
     Q_UNUSED(config);
     return nullptr;
 }
 
-QUrl Cargo::workingDirectory(KDevelop::ILaunchConfiguration* config) const
+QUrl CargoPlugin::workingDirectory(KDevelop::ILaunchConfiguration* config) const
 {
     return config->project()->path().toUrl();
 }
 
-QString Cargo::environmentGroup(KDevelop::ILaunchConfiguration* /*config*/) const
+QString CargoPlugin::environmentGroup(KDevelop::ILaunchConfiguration* /*config*/) const
 {
     return QString();
 }
 
-QString Cargo::nativeAppConfigTypeId() const
+QString CargoPlugin::nativeAppConfigTypeId() const
 {
     return CargoExecutionConfigType::typeId();
 }
 
-bool Cargo::useTerminal(KDevelop::ILaunchConfiguration* /*config*/) const
+bool CargoPlugin::useTerminal(KDevelop::ILaunchConfiguration* /*config*/) const
 {
     return false;
 }
 
-QString Cargo::terminal(KDevelop::ILaunchConfiguration* /*config*/) const
+QString CargoPlugin::terminal(KDevelop::ILaunchConfiguration* /*config*/) const
 {
     return QString();
 }
